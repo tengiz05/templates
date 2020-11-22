30822c1c80116075f253b4ca6a70efd4478626ff
tengiz05 <66938315+tengiz05@users.noreply.github.com>
2020-11-22 22:36:39 +0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define pii pair<int, int>
+#define ff first
+#define ss second
+#define PI acos(-1)
+#define ld long double
+const int mod = 1e9+7, N = 2e5+5;
+int msb(int val){return sizeof(int)*8-__builtin_clzll(val);}
+int a[N], n, m, k;
+
+void solve(int test_case){
+	int i, j;
+	
+	return;
+}
+
+signed main(){
+	FASTIO;
+#define MULTITEST 1
+#if MULTITEST
+	int ___T;
+	cin >> ___T;
+	for(int T_CASE = 1; T_CASE <= ___T; T_CASE++)
+		solve(T_CASE);
+#else
+	solve(1);
+#endif
+	return 0;
+}
