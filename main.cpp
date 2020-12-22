3ea3bfa324cf38762c469bea1dcc88224abc51ef
tengiz05 <66938315+tengiz05@users.noreply.github.com>
2020-12-22 14:20:17 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
-#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL);
 #define all(x) (x).begin(), (x).end()
 #define pb push_back
 #define pii pair<int, int>
@@ -9,8 +9,10 @@ using namespace std;
 #define ss second
 #define PI acos(-1)
 #define ld long double
+template <class T> bool ckmin(T& a, const T& b) {return a > b ? a = b, true : false;}
+template <class T> bool ckmax(T& a, const T& b) {return a < b ? a = b, true : false;}
 const int mod = 1e9+7, N = 2e5+5;
-int msb(int val){return sizeof(int)*8-__builtin_clzll(val);}
+int msb(int val){return sizeof(int)*8-__builtin_clzll(val)-1;}
 int a[N], n, m, k;
 
 void solve(int test_case){
@@ -23,12 +25,15 @@ signed main(){
 	FASTIO;
 #define MULTITEST 1
 #if MULTITEST
-	int ___T;
-	cin >> ___T;
-	for(int T_CASE = 1; T_CASE <= ___T; T_CASE++)
+	int _T;
+	cin >> _T;
+	for(int T_CASE = 1; T_CASE <= _T; T_CASE++)
 		solve(T_CASE);
 #else
 	solve(1);
 #endif
 	return 0;
 }
+
+
+
