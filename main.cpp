63a639206bc0b839eb2e4132644098aada93a118
tengiz05 <66938315+tengiz05@users.noreply.github.com>
2021-07-07 21:10:06 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,40 +1,7 @@
 #include <bits/stdc++.h>
-using namespace std;
-#define int long long
-#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL);
-#define all(x) (x).begin(), (x).end()
-#define pb push_back
-#define pii pair<int, int>
-#define ff first
-#define ss second
-#define PI acos(-1)
-#define ld long double
-template<class T> bool ckmin(T& a, const T& b) {return a>b? a=b, true:false;}
-template<class T> bool ckmax(T& a, const T& b) {return a<b? a=b, true:false;}
-const int mod = 1e9+7, N = 2e5+5;
-int msb(int val){return sizeof(int)*8-__builtin_clzll(val)-1;}
-int a[N], n, m, k;
-
-void solve(int test_case){
-	int i, j;
-	
-	return;
+using i64 = long long;
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    return 0;
 }
-
-signed main(){
-	FASTIO;
-#define MULTITEST 1
-#if MULTITEST
-	int _T;
-	cin >> _T;
-	for(int T_CASE = 1; T_CASE <= _T; T_CASE++)
-		solve(T_CASE);
-#else
-	solve(1);
-#endif
-	return 0;
-}
-
-
-
-
