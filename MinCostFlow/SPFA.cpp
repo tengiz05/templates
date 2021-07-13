964e06ddaef91d902cc13953d2f074601ae8eb49
tengiz05 <66938315+tengiz05@users.noreply.github.com>
2021-07-13 16:42:40 +0600

    Update SPFA.cpp

--- MinCostFlow/SPFA.cpp
+++ MinCostFlow/SPFA.cpp
@@ -1,30 +1,36 @@
 struct MinCostFlow {
     static constexpr int INF = 1e9;
     struct Edge {
-        int from, to, cap, cost;
+        int to, cap, cost;
     };
     int n;
-    std::vector<Edge> edges;
-    std::vector<std::vector<int>> adj, cost, cap;
-    MinCostFlow(int n) : n(n) {}
+    std::vector<Edge> e;
+    std::vector<std::vector<int>> g;
+    MinCostFlow(int n) : n(n), g(n) {}
     void addEdge(int u, int v, int cap, int cost) {
-        edges.push_back({u, v, cap, cost});
+        g[u].push_back(e.size());
+        e.push_back({v, cap, cost});
+        g[v].push_back(e.size());
+        e.push_back({u, 0, -cost});
     }
-    void shortest_paths(int s, std::vector<int>& d, std::vector<int>& p) {
+    void SPFA(int s, std::vector<int>& d, std::vector<int>& p, std::vector<int> &par) {
         d.assign(n, INF);
         d[s] = 0;
         std::vector<bool> inq(n, false);
         std::queue<int> que;
         que.push(s);
         p.assign(n, -1);
+        par.assign(n, -1);
         while (!que.empty()) {
             int u = que.front();
             que.pop();
             inq[u] = false;
-            for (int v : adj[u]) {
-                if (cap[u][v] > 0 && d[v] > d[u] + cost[u][v]) {
-                    d[v] = d[u] + cost[u][v];
-                    p[v] = u;
+            for (int i : g[u]) {
+                auto [v, cap, cost] = e[i];
+                if (cap > 0 && d[v] > d[u] + cost) {
+                    d[v] = d[u] + cost;
+                    p[v] = i;
+                    par[v] = u;
                     if (!inq[v]) {
                         inq[v] = true;
                         que.push(v);
@@ -33,37 +39,27 @@ struct MinCostFlow {
             }
         }
     }
-    int min_cost_flow(int K, int s, int t) {
-        adj.assign(n, std::vector<int>());
-        cost.assign(n, std::vector<int>(n, 0));
-        cap.assign(n, std::vector<int>(n, 0));
-        for (Edge e : edges) {
-            adj[e.from].push_back(e.to);
-            adj[e.to].push_back(e.from);
-            cost[e.from][e.to] = e.cost;
-            cost[e.to][e.from] = -e.cost;
-            cap[e.from][e.to] = e.cap;
-        }
+    int minCost(int K, int s, int t) {
         int flow = 0;
         int cost = 0;
-        std::vector<int> d, p;
+        std::vector<int> d, p, par;
         while (flow < K) {
-            shortest_paths(s, d, p);
+            SPFA(s, d, p, par);
             if (d[t] == INF)
                 break;
             int f = K - flow;
             int cur = t;
             while (cur != s) {
-                f = std::min(f, cap[p[cur]][cur]);
-                cur = p[cur];
+                f = std::min(f, e[p[cur]].cap);
+                cur = par[cur];
             }
             flow += f;
             cost += f * d[t];
             cur = t;
             while (cur != s) {
-                cap[p[cur]][cur] -= f;
-                cap[cur][p[cur]] += f;
-                cur = p[cur];
+                e[p[cur]].cap -= f;
+                e[p[cur] ^ 1].cap += f;
+                cur = par[cur];
             }
         }
         if (flow < K)
