70b5350c34be2ed40789ef03253d127f4c521ad3
tengiz05 <66938315+tengiz05@users.noreply.github.com>
2021-07-13 16:50:48 +0600

    Create Flow_i64.cpp

---
+++ MaxFlow/Flow_i64.cpp
@@ -0,0 +1,65 @@
+struct Flow {
+    static constexpr i64 INF = 1e18;
+    int n;
+    struct Edge {
+        int to;
+        i64 cap;
+        Edge(int to, i64 cap) : to(to), cap(cap) {}
+    };
+    std::vector<Edge> e;
+    std::vector<std::vector<int>> g;
+    std::vector<int> cur, h;
+    Flow(int n) : n(n), g(n) {}
+    bool bfs(int s, int t) {
+        h.assign(n, -1);
+        std::queue<int> que;
+        h[s] = 0;
+        que.push(s);
+        while (!que.empty()) {
+            int u = que.front();
+            que.pop();
+            for (int i : g[u]) {
+                auto [v, c] = e[i];
+                if (c > 0 && h[v] == -1) {
+                    h[v] = h[u] + 1;
+                    if (v == t)
+                        return true;
+                    que.push(v);
+                }
+            }
+        }
+        return false;
+    }
+    i64 dfs(int u, int t, i64 f) {
+        if (u == t)
+            return f;
+        i64 r = f;
+        for (int &i = cur[u]; i < int(g[u].size()); ++i) {
+            int j = g[u][i];
+            auto [v, c] = e[j];
+            if (c > 0 && h[v] == h[u] + 1) {
+                i64 a = dfs(v, t, std::min(r, c));
+                e[j].cap -= a;
+                e[j ^ 1].cap += a;
+                r -= a;
+                if (r == 0)
+                    return f;
+            }
+        }
+        return f - r;
+    }
+    void addEdge(int u, int v, int c) {
+        g[u].push_back(e.size());
+        e.emplace_back(v, c);
+        g[v].push_back(e.size());
+        e.emplace_back(u, 0);
+    }
+    i64 maxFlow(int s, int t) {
+        i64 ans = 0;
+        while (bfs(s, t)) {
+            cur.assign(n, 0);
+            ans += dfs(s, t, INF);
+        }
+        return ans;
+    }
+};
