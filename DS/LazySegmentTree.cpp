2fb7ec7e8982901399101bb80a9b99692532c790
tengiz05 <66938315+tengiz05@users.noreply.github.com>
2021-10-11 14:08:59 +0600

    Create LazySegmentTree.cpp

---
+++ DS/LazySegmentTree.cpp
@@ -0,0 +1,87 @@
+template<class Info, class Tag,
+    class Merge = std::plus<Info>>
+struct LazySegmentTree {
+    const int n;
+    const Merge merge;
+    std::vector<Info> info;
+    std::vector<Tag> tag;
+    LazySegmentTree(int n) : n(n), merge(Merge()), info(4 << std::__lg(n)), tag(4 << std::__lg(n)) {}
+    LazySegmentTree(std::vector<Info> init) : LazySegmentTree(init.size()) {
+        std::function<void(int, int, int)> build = [&](int p, int l, int r) {
+            if (r - l == 1) {
+                info[p] = init[l];
+                return;
+            }
+            int m = (l + r) / 2;
+            build(2 * p, l, m);
+            build(2 * p + 1, m, r);
+            pull(p);
+        };
+        build(1, 0, n);
+    }
+    void pull(int p) {
+        info[p] = merge(info[2 * p], info[2 * p + 1]);
+    }
+    void apply(int p, const Tag &v) {
+        ::apply(info[p], v);
+        ::apply(tag[p], v);
+    }
+    void push(int p) {
+        apply(2 * p, tag[p]);
+        apply(2 * p + 1, tag[p]);
+        tag[p] = Tag();
+    }
+    void modify(int p, int l, int r, int x) {
+        if (r - l == 1) {
+            info[p].cnt = 1;
+            if ((x + tag[p]) % 2 == 0) {
+                info[p].x[1][0] += 1E9 + x;
+            } else {
+                info[p].x[1][1] += 1E9 + x;
+            }
+            return;
+        }
+        int m = (l + r) / 2;
+        push(p);
+        if (x < m) {
+            modify(2 * p, l, m, x);
+        } else {
+            modify(2 * p + 1, m, r, x);
+        }
+        pull(p);
+    }
+    void modify(int p) {
+        modify(1, 0, n, p);
+    }
+    Info rangeQuery(int p, int l, int r, int x, int y) {
+        if (l >= y || r <= x) {
+            return Info();
+        }
+        if (l >= x && r <= y) {
+            return info[p];
+        }
+        int m = (l + r) / 2;
+        push(p);
+        return merge(rangeQuery(2 * p, l, m, x, y), rangeQuery(2 * p + 1, m, r, x, y));
+    }
+    Info rangeQuery(int l, int r) {
+        return rangeQuery(1, 0, n, l, r);
+    }
+    void rangeApply(int p, int l, int r, int x, int y, const Tag &v) {
+        if (l >= y || r <= x) {
+            return;
+        }
+        if (l >= x && r <= y) {
+            apply(p, v);
+            return;
+        }
+        int m = (l + r) / 2;
+        push(p);
+        rangeApply(2 * p, l, m, x, y, v);
+        rangeApply(2 * p + 1, m, r, x, y, v);
+        pull(p);
+    }
+    void rangeApply(int l, int r, const Tag &v) {
+        return rangeApply(1, 0, n, l, r, v);
+    }
+};
