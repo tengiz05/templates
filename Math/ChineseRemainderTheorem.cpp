3da865d18ec930d5246eeb53c702623a5fd5569d
tengiz05 <66938315+tengiz05@users.noreply.github.com>
2021-10-21 14:30:12 +0600

    Create ChineseRemainderTheorem.cpp

---
+++ Math/ChineseRemainderTheorem.cpp
@@ -0,0 +1,32 @@
+constexpr i64 inf = 1LL << 62;
+
+i64 power(i64 a, i64 b, i64 P) {
+    i64 res = 1;
+    for (; b > 0; b /= 2, a = __int128(a) * a % P) {
+        if (b % 2 == 1) {
+            res = __int128(res) * a % P;
+        }
+    }
+    return res;
+}
+
+std::mt19937 rnd(std::chrono::steady_clock::now().time_since_epoch().count());
+bool prime(i64 n) {
+    if (n < 5)
+        return (n == 2 || n == 3);
+    if (n % 2 == 0)
+        return false;
+    bool prime = false;
+    for (int i = 0; i < 20; i++) {
+        i64 a = rnd() % inf;
+        a = a % (n - 2) + 2;
+        i64 p = power(a, (n - 1) / 2, n);
+        if (p == 1)
+            continue;
+        if (p == n - 1)
+            prime = true;
+        else 
+            return false;
+    }
+    return prime;
+}
