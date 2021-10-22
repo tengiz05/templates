cc38f77c44fc2494895531c411a21d5d163f53a4
tengiz05 <66938315+tengiz05@users.noreply.github.com>
2021-10-22 11:56:10 +0600

    Update PrimeCheck.cpp

--- Math/PrimeCheck.cpp
+++ Math/PrimeCheck.cpp
@@ -1,5 +1,4 @@
 constexpr i64 inf = 1LL << 62;
-
 i64 power(i64 a, i64 b, i64 P) {
     i64 res = 1;
     for (; b > 0; b /= 2, a = __int128(a) * a % P) {
@@ -9,17 +8,17 @@ i64 power(i64 a, i64 b, i64 P) {
     }
     return res;
 }
-
 std::mt19937 rnd(std::chrono::steady_clock::now().time_since_epoch().count());
+i64 pr[12] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37};
 bool prime(i64 n) {
     if (n < 5)
         return (n == 2 || n == 3);
     if (n % 2 == 0)
         return false;
     bool prime = false;
-    for (int i = 0; i < 20; i++) {
-        i64 a = rnd() % inf;
-        a = a % (n - 2) + 2;
+    for (int i = 0; i < 12; i++) {
+        i64 a = pr[i];
+        if (a >= n) break;
         i64 p = power(a, (n - 1) / 2, n);
         if (p == 1)
             continue;
