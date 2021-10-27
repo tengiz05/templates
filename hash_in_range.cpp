ff6bf9950eb23c5e4856cfc82cc0bc7f1da21bfe
tengiz05 <66938315+tengiz05@users.noreply.github.com>
2021-10-27 16:29:47 +0600

    Update hash_in_range.cpp

--- hash_in_range.cpp
+++ hash_in_range.cpp
@@ -1,19 +1,17 @@
-struct Hash{
-	const int P = 29, mod = 1e9+7;
-	int n;
-	vector<int> a, pw;
-	Hash(string &s){
-		n = s.size();
-		a.resize(n,0);
-		pw.resize(n+1,1);
-		for(int i=1;i<=n;i++)pw[i] = (long long)pw[i-1]*P%mod;
-		a[0] = s[0]-'A'+1;
-		for(int i=1;i<n;i++){
-			a[i] = ((long long)a[i-1]*P + (s[i]-'A'+1))%mod;
-		}
-	}
-	int get(int l, int r){
-		if(l == 0)return a[r];
-		return ((a[r] - ((long long)a[l-1]*pw[r-l+1])%mod)%mod+mod)%mod;
-	}
+struct Hash {
+    static constexpr int P = 1000000007, M = 29;
+    int n;
+    std::vector<int> a, pw;
+    Hash(std::string s) : n(s.size()), a(n + 1), pw(n + 1, 1) {
+        for (int i = 1; i <= n; i++)
+            pw[i] = 1LL * pw[i - 1] * M % P;
+        for (int i = 0; i < n; i++)
+            a[i + 1] = (1LL * a[i] * M + (s[i] - 'a' + 1)) % P;
+    }
+    int get(int l, int r) {
+        int res = a[r] - 1LL * pw[r - l] * a[l] % P;
+        if (res < 0)
+            res += P;
+        return res;
+    }
 };
