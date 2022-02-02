1d969d31de93f2c8395563055fbfae22f8b27aa7
tengiz05 <66938315+tengiz05@users.noreply.github.com>
2022-02-02 16:29:06 +0600

    add compare function

--- StringAlgorithms/SuffixArray.cpp
+++ StringAlgorithms/SuffixArray.cpp
@@ -70,3 +70,12 @@ std::vector<int> suffix_array(std::string s) {
     shifts.erase(shifts.begin());
     return shifts;
 }
+
+
+
+int compare(int i, int j, int l) {
+    int k = std::__lg(l);
+    std::pair<int, int> a = {c[k][i], c[k][(i + l - (1 << k)) % n]};
+    std::pair<int, int> b = {c[k][j], c[k][(j + l - (1 << k)) % n]};
+    return a == b ? 0 : a < b ? -1 : 1;
+}
