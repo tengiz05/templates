b1d33dca18ce0209b3a8e6c873f56d1c5259a480
tengiz05 <66938315+tengiz05@users.noreply.github.com>
2022-06-28 00:51:21 +0600

    Create PrefixFunction.cpp

---
+++ StringAlgorithms/PrefixFunction.cpp
@@ -0,0 +1,13 @@
+std::vector<int> prefix_function(std::string s) {
+    int n = s.size();
+    std::vector<int> pi(n);
+    for (int i = 1; i < n; i++) {
+        int j = pi[i - 1];
+        while (j > 0 && s[i] != s[j])
+            j = pi[j - 1];
+        if (s[i] == s[j])
+            j++;
+        pi[i] = j;
+    }
+    return pi;
+}
